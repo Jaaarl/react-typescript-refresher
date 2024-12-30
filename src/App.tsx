44f401e055f15c28e43a46c2536229cc80d6c1ab
jaaarl <jarl13.work@gmail.com>
2024-12-30 19:18:51 +0800

    add: used props for user list card

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
 function App() {
 
-  return (
-    <>
-      <div className={"text-3xl text-red-800"}>Hello world</div>
-    </>
-  )
+	return (
+		<>
+			<h1 className={"font-bold"}>User list</h1>
+			<div className={"flex flex-col gap-4"}>
+				<UserCard name="John Doe" email="johndoe@gmail.com"/>
+				<UserCard name="Rey Valera" email="reyvalera@gmail.com"/>
+				<UserCard name="Sukona Jujutsu" email="sukona@gmail.com"/>
+			</div>
+		</>
+	)
 }
 
 export default App
+
+type UserCardProps = {
+	name: string;
+	email: string;
+}
+
+function UserCard(props: UserCardProps) {
+	return (
+		<>
+			<div className={"flex flex-col gap-2 p-2 bg-gray-200 max-w-sm"}>
+				<h1> name: {props.name} </h1>
+				<h1> email: {props.email} </h1>
+			</div>
+		</>
+	)
+}
